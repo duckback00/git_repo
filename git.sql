c57b4680e6dc3d8423a69dbb9b8927088adc3828
duckback00 <alan.bitterman@unitask.com>
2012-01-24 10:14:33 -0500

    Update git.sql

--- git.sql
+++ git.sql
@@ -1,2 +1,8 @@
-select count(*) from all_objects;
-select sysdate from dual;
+create or replace procedure hello as
+v_cnt   PLS_INTEGER;
+begin
+   select count(*) into v_cnt from all_objects;
+   --select sysdate from dual;
+   dbms_output.put_line('Hello from GIT ... '||v_cnt);
+end;
+/
