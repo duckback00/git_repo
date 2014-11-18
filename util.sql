ea3c0f4788db05275d48c45738d00179a33f1ec1
Alan Bitterman <alan@unitask.com>
2014-11-18 08:28:40 -0500

    Unitask OMM: No remarks provided for CHECK_IN operation

---
+++ util.sql
@@ -0,0 +1,9 @@
+REM $Id: util.sql 930 2009-01-04 11:39:22Z mshapira $
+REM aporter
+REM Copyright Unitask Inc., 1999-2006. All rights reserved
+set head off
+set trim off
+set long 1000000
+set wrap off
+set feedback off
+set linesize 2000
